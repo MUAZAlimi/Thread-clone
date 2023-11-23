af130f975f51134902d778980198d9bc392edc98
MUAZAlimi <112557869+MUAZAlimi@users.noreply.github.com>
2023-11-23 15:46:52 +0100

    Initialize project using Create React App

--- src/component/UserHeader.js
+++ src/component/UserHeader.js
@@ -1,5 +1,7 @@
-import { Box, Flex, VStack, Text } from "@chakra-ui/layout";
+import { Box, Flex, VStack, Text, Link } from "@chakra-ui/layout";
 import { Avatar } from "@chakra-ui/react";
+import { CgMoreO } from "react-icons/cg"
+import { BsInstagram } from "react-icons/bs";
 
 const UserHeader = () => {
   return (
@@ -7,7 +9,7 @@ const UserHeader = () => {
       <Flex justifyContent={"space-between"} w={"full"}>
         <Box>
           <Text fontSize={"2xl"} fontWeight={"bold"}>
-            Alimi Muaz
+            Alimi Muaz .O
           </Text>
           <Flex gap={2} align={"center"}>
             <Text fontSize={"sm"}>yungalimzy123</Text>
@@ -26,7 +28,13 @@ const UserHeader = () => {
             <Text>FrontEnd developer of DltAfrica Chort 3.0</Text>
             <Flex w={"full"} justifyContent={"space-between"}>
                 <Flex gap={2} alignItems={"center"}>
-                    
+                    <Text color={"gray.light"}>4.5m followers</Text>
+                    <Box w={1} h={1} bg={"gray.light"} borderRadius="50%"></Box>
+                    <Link color={"gray.light"}>instagram.com</Link>
+                </Flex>
+                <Flex gap={2}>
+                      <Box><BsInstagram size={24} cursor={"pointer"}/></Box>
+                        <Box><CgMoreO size={24} cursor={"pointer"}/></Box>
                 </Flex>
             </Flex>
     </VStack>
