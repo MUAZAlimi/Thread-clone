e4bdc78366902f299c745f15fa34cd426bca0802
MUAZAlimi <112557869+MUAZAlimi@users.noreply.github.com>
2023-11-23 15:15:42 +0100

    Initialize project using Create React App

--- src/component/UserHeader.js
+++ src/component/UserHeader.js
@@ -1,13 +1,36 @@
-import { Box, Flex, VStack,Text } from "@chakra-ui/layout";
+import { Box, Flex, VStack, Text } from "@chakra-ui/layout";
+import { Avatar } from "@chakra-ui/react";
 
 const UserHeader = () => {
-  return <VStack gap={4} alignItems={"start"}>
+  return (
+    <VStack gap={4} alignItems={"start"}>
       <Flex justifyContent={"space-between"} w={"full"}>
         <Box>
-            <Text>Alimi Muaz</Text>
+          <Text fontSize={"2xl"} fontWeight={"bold"}>
+            Alimi Muaz
+          </Text>
+          <Flex gap={2} align={"center"}>
+            <Text fontSize={"sm"}>yungalimzy123</Text>
+            <Text fontSize={"xm"} bg={"gray.dark"} color={"gray.light"} p={1} borderRadius={"full"}>thread.net</Text>
+          </Flex>
+        </Box>
+
+        <Box>
+            <Avatar 
+            name="Alimi Muaz"
+            src="/directorPro.jpeg"
+            size={"xl"}/>
         </Box>
       </Flex>
+
+            <Text>FrontEnd developer of DltAfrica Chort 3.0</Text>
+            <Flex w={"full"} justifyContent={"space-between"}>
+                <Flex gap={2} alignItems={"center"}>
+                    
+                </Flex>
+            </Flex>
     </VStack>
+  );
 };
 
 export default UserHeader;
