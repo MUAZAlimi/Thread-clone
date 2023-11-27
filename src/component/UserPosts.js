be56e5f9ed6745e963ef3955536476245a00269e
MUAZAlimi <112557869+MUAZAlimi@users.noreply.github.com>
2023-11-27 11:16:11 +0100

    Initialize project using Create React App

--- src/component/UserPosts.js
+++ src/component/UserPosts.js
@@ -1,17 +1,57 @@
-import { Avatar } from "@chakra-ui/avatar";
-import { Flex } from "@chakra-ui/layout";
+import { Avatar, Box, Flex, Image, Text } from "@chakra-ui/react";
+import { BsThreeDots } from "react-icons/bs";
 import { Link } from "react-router-dom";
 
 const UserPosts = () => {
   return (
     <Link to={"/yungalimzy/post/1"}>
-      <Flex>
-        <Flex>
-            <Avatar></Avatar>    
-        </Flex>        
+      <Flex gap={3} mb={4} py={5}>
+        <Flex flexDir={"column"}>
+          <Avatar src="/post1.png" name="Mark Zuckerberg" size={"md"} />
+          <Box w={"1px"} h={"full"} bg={"gray.light"} my={2}></Box>
+          <Box pos={"relative"} w={"full"}>
+            <Avatar
+              src="/post1.png"
+              name="Wale Oloyin"
+              size={"xs"}
+              pos={"absolute"}
+              top={"0"}
+              left={"15px"}
+              padding={"2px"}
+            />
+            <Avatar
+              src="/post1.png"
+              name="Bella Shmurda"
+              size={"xs"}
+              pos={"absolute"}
+              bottom={"0"}
+              right={"-5px"}
+              padding={"2px"}
+            />
+            <Avatar
+              src="/post1.png"
+              name="Danfo Driver"
+              size={"xs"}
+              pos={"absolute"}
+              bottom={"0"}
+              left={"4px"}
+              padding={"2px"}
+            />
+          </Box>
+        </Flex>
+        <Flex flex={1} flexDir={"column"} w={"full"}>
+          <Flex alignItems={"center"} w={"full"}>
+            <Text>AlimiMuaz</Text>
+            <Image src="/verified.png" ml={1} w={4} h={4} />
+          </Flex>
+          <Flex alignItems={"center"} gap={4}>
+            <Text>1d</Text>
+            <BsThreeDots />
+          </Flex>
+        </Flex>
       </Flex>
     </Link>
-  )
-}
+  );
+};
 
-export default UserPosts
+export default UserPosts;
