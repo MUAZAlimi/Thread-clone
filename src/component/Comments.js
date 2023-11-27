c1e85f1feaaef53650ece4169c75a48a677137fc
MUAZAlimi <112557869+MUAZAlimi@users.noreply.github.com>
2023-11-27 04:03:07 +0100

    Initialize project using Create React App

--- src/component/Comments.js
+++ src/component/Comments.js
@@ -1,5 +1,6 @@
-import { Avatar, Flex } from "@chakra-ui/react";
+import { Avatar, Flex, Text } from "@chakra-ui/react";
 import React from "react";
+import { BsThreeDots } from "react-icons/bs";
 
 const Comments = ({ comment, createAt, likes, username, userAvater }) => {
   return (
@@ -10,6 +11,19 @@ const Comments = ({ comment, createAt, likes, username, userAvater }) => {
           size={"sm"}
           name="Ryan Florence"
         />
+        <Flex flexDir={"column"} gap={1} w={"full"} border={"2px solid white"}>
+          <Flex
+            justifyContent={"space-between"}
+            alignItems={"center"}
+            w={"full"}
+          >
+            <Text fontSize={"sm"} fontWeight={"bold"}>John Doe</Text>
+          <Flex gap={2} alignItems={"center"}>
+                <Text fontSize={"sm"} color={"gray.light"}>2 min ago</Text>
+                <BsThreeDots/>
+          </Flex>
+          </Flex>
+        </Flex>
       </Flex>
     </React.Fragment>
   );
