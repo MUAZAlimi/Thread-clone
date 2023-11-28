07709d57a51164a23be55bafe7e77d2c781a943b
MUAZAlimi <112557869+MUAZAlimi@users.noreply.github.com>
2023-11-28 11:27:18 +0100

    Initial commit

---
+++ thread/thread-backend/db/connectDb.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose"
+
+const connectDb = async () => {
+   try {
+   const conn =  await mongoose.connect(process.env.MONGO_URI, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true 
+    });
+    console.log(`MongoDB connected 👋👋`)
+   } catch (err) {
+    console.error(`Error: ${eror.message}`);
+    process.exit(1)
+   }
+}
+
+export default connectDb;
--- thread/thread-backend/server.js
+++ thread/thread-backend/server.js
@@ -1,8 +1,10 @@
 import express from "express"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
+import connectDb from "./db/connectdb";
 
 dotenv.config();
+connectDb()
 const app = express()
 
 const PORT = process.env.Port || 5000;
