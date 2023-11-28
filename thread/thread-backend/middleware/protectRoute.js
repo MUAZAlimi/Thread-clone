1d38f891bd89cdef7861415cab08fdab6731cced
MUAZAlimi <112557869+MUAZAlimi@users.noreply.github.com>
2023-11-28 12:47:08 +0100

    Initial commit

---
+++ thread/thread-backend/middleware/protectRoute.js
@@ -0,0 +1,14 @@
+import jwt from "jsonwebtoken";
+
+const protectRoute = async (req, res, next) => {
+    try {
+        const token = req.cookies.jwt;
+        if(!token) return res.status(401).json
+    } catch (error) {
+        res.status(500).json({ message: error.message});
+        console.log('Error while signing up user :', error.message)
+    }
+}
+
+
+export default protectRoute
