0a62b2fd37f4c627457c375f2ab5c863d025c0c3
MUAZAlimi <112557869+MUAZAlimi@users.noreply.github.com>
2023-11-29 09:43:58 +0100

    up

--- thread/routes/userRoutes.js
+++ thread/routes/userRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { signUPUser } = require("../contollers/userController");
-
+const { signUPUser , loginUser } = require("../contollers/userController");
+ 
 const router = express.Router();
 
 router.post("/signup", signUPUser);
+router.post("/login", loginUser);
 
 module.exports = router;
