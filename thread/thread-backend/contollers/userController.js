5a63035ba2d08bf6a9af846a0ce9f0d1adf7c3d0
MUAZAlimi <112557869+MUAZAlimi@users.noreply.github.com>
2023-11-29 09:14:01 +0100

    update

--- thread/thread-backend/contollers/userController.js
+++ thread/thread-backend/contollers/userController.js
@@ -1,7 +1,45 @@
+const User = require("../models/useModel");
+const bcrypt = require("bcryptjs");
+const generateTokenAndSetCookie = require("../utils/helper/generateTokenAndSetCookie");
+
 const signUPUser = async (req, res) => {
-    res.send("User Signed up Successfully")
-}
+  try {
+    const { name, email, username, password } = req.body;
+    const user = await User.findOne({ $or: [{ email }, { username }] });
+
+    if (user) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      name,
+      email,
+      username,
+      password: hashedPassword,
+    });
+
+    await newUser.save();
+
+    if (newUser) {
+      generateTokenAndSetCookie(newUser._id, res);
+      res.status(201).json({
+        _id: newUser._id,
+        name: newUser.name,
+        email: newUser.email,
+        username: newUser.username,
+      });
+    } else {
+      res.status(400).json({ message: "Invalid user data" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+    console.log("Error in ", error.message);
+  }
+};
 
 module.exports = {
-    signUPUser
-}
+  signUPUser,
+};
---
+++ thread/thread-backend/utils/helper/generateTokenAndSetCookie.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken')
+
+const generateTokenAndSetCookie = (userId, res) => {
+    const token = jwt.sign({userId},process.env.JWT_SECRET, {
+        expiresIn: '15d'
+    })
+
+    res.cookie('jwt', token, {
+
+        httpOnly: true, // more secure
+        maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days
+        sameSite: "strict", // CSRF
+    })
+
+    return token
+}
+
+
+module.exports = generateTokenAndSetCookie
