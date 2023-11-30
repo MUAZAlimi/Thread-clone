1a23fee89fb8e765b9f18853d3bf8f1b26a64c52
MUAZAlimi <112557869+MUAZAlimi@users.noreply.github.com>
2023-11-30 12:33:45 +0100

    update

--- thread/contollers/postController.js
+++ thread/contollers/postController.js
@@ -55,7 +55,19 @@ const getPost = async (req, res) => {
 
 const deletePost = async (req, res) => {
 try {
-    
+    const post = await Post.findById(req.params.id)
+
+    if(!post){
+         return res.status(404).json({message: "post not found"})
+    }
+
+    if(post.postedBy.toString() !== req.user._id.toString()){
+        return res.status(404).json({message: "Unauthorized to delete  this post"})
+    }
+
+    await Post.findByIdAndDelete(req.params.id)
+
+    res.status(200).json({message: "post deleted successfully"})
 } catch (err) {
     res.status(500).json({ message: err.message });
     console.log("Error in Delete post:", err.message);
