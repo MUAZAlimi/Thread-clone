7b9693e590f3d3a8ce3e5100c8b9d3121ce47c4b
MUAZAlimi <112557869+MUAZAlimi@users.noreply.github.com>
2023-11-30 11:50:35 +0100

    update

--- thread/routes/postRoutes.js
+++ thread/routes/postRoutes.js
@@ -4,7 +4,7 @@ const protectRoute = require("../middleware/protectRoute");
 
 const router = express.Router()
 
-
+router.get("/", getPost)
 router.post("/create", protectRoute, createPost)
 
 module.exports = router
