f8a18c86cd634409db6cb76fd9faef5d2a1d7f7a
MUAZAlimi <112557869+MUAZAlimi@users.noreply.github.com>
2023-11-30 11:11:56 +0100

    update

--- thread/contollers/postController.js
+++ thread/contollers/postController.js
@@ -1,5 +1,5 @@
+const Post = require("../models/postModel");
 const User = require("../models/useModel");
-
 const createPost = async (req, res) => {
   try {
     const { postedBy, text, img } = req.body;
@@ -13,8 +13,23 @@ const createPost = async (req, res) => {
     const user = await User.findById(postedBy);
 
     if (!user) {
-        return res.status(404).json({message: "user not found"})
+      return res.status(404).json({ message: "user not found" });
     }
+
+    const maxLength = 500;
+
+    if (text.length > maxLength) {
+      return res
+        .status(400)
+        .json({ message: `Text must be less than ${maxLength} characters` });
+    }
+    const newPost = new Post({postedBy, text, img});
+
+    await newPost.save()
+
+    res.status(201).json({ message: "Post created successfully", newPost})
+
+
   } catch (err) {}
   res.status(500).json({ message: err.message });
   console.log("Error in Create post", err.message);
