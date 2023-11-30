31079d72e5addfff51638b1bee92520263ec28ea
MUAZAlimi <112557869+MUAZAlimi@users.noreply.github.com>
2023-11-30 15:54:48 +0100

    update

--- thread/contollers/postController.js
+++ thread/contollers/postController.js
@@ -133,10 +133,21 @@ const replyToPost = async (req, res) => {
 
 const getFeedPost = async (req, res) => {
     try {
+        const userId = req.user._id
+        const user = await User.findById(userId)
         
+        if (!user) {
+            return res.status(404).jso({message: "User not found"})
+        }
+
+        const following = user.following
+
+        const feedPosts = await Post.find({postedBy: {$in: following}}).sort({createAt: -1});
+
+        res.status(200).json({feedPosts})
     } catch (err) {
         res.status(500).json({ message: err.message });
-        console.log("Error in ReplyToPost:", err.message);
+        console.log("Error in FeedPost:", err.message);
     }
 }
 
