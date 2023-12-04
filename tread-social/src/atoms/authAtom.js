80ca0566b612e1dc738a234d18866aa5c716897f
MUAZAlimi <112557869+MUAZAlimi@users.noreply.github.com>
2023-12-04 11:33:00 +0100

    UPDATE

--- tread-social/src/atoms/authAtom.js
+++ tread-social/src/atoms/authAtom.js
@@ -1,1 +1,8 @@
+import { atom } from "recoil"
 
+const authScreenAtom= atom({
+    key: "autScreenAtom",
+    default: "login"
+})
+
+export default authScreenAtom
--- tread-social/src/pages/AuthPage.js
+++ tread-social/src/pages/AuthPage.js
@@ -1,11 +1,15 @@
 import React from "react";
 import LoginCard from "../component/LoginCard";
 import SignUpCard from "../component/SignUpCard";
+import { useRecoilValue } from "recoil";
+import authScreenAtom from "../atoms/authAtom";
 
 const AuthPage = () => {
+    const authScreenState = useRecoilValue(authScreenAtom)
+
   return (
     <>
-      "login" ? <LoginCard /> : <SignUpCard />{" "}
+      {authScreenState === "login" ? <LoginCard /> : <SignUpCard /> }
     </>
   );
 };
