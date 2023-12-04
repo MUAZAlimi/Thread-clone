c2ea3c8e52bdbd919bf411f6d1a28a9867fb05c0
MUAZAlimi <112557869+MUAZAlimi@users.noreply.github.com>
2023-12-04 10:30:00 +0100

    update

--- tread-social/src/component/LoginCard.js
+++ tread-social/src/component/LoginCard.js
@@ -29,7 +29,7 @@ export default function LoginCard() {
       <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
         <Stack align={'center'}>
           <Heading fontSize={'4xl'} textAlign={'center'}>
-            Sign up
+            Login
           </Heading>
           <Text fontSize={'lg'} color={'gray.600'}>
             to enjoy all of our cool features ✌️
