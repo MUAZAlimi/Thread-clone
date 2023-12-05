993a430796eb4d44423070eb657833d252039754
MUAZAlimi <112557869+MUAZAlimi@users.noreply.github.com>
2023-12-05 09:13:38 +0100

    update

--- tread-social/src/component/LogoutButton.js
+++ tread-social/src/component/LogoutButton.js
@@ -1,12 +1,12 @@
-import { Button } from "@chakra-ui/react"
-
+import { Button } from "@chakra-ui/react";
+import { AiOutlineLogout } from "react-icons/ai";
 
 const LogoutButton = () => {
   return (
-    <Button position={"fixed"} top={"30px"} right={"30px"} size={"sm"}> 
-      LogoutButton
+    <Button position={"fixed"} top={"30px"} right={"30px"} size={"sm"}>
+      <AiOutlineLogout size={20} />
     </Button>
-  )
-}
+  );
+};
 
-export default LogoutButton
+export default LogoutButton;
