568300ce56ae70f6477fd38962c4703d01f2a9e6
MUAZAlimi <112557869+MUAZAlimi@users.noreply.github.com>
2023-12-08 12:53:54 +0100

    update

--- tread-social/src/component/Post.js
+++ tread-social/src/component/Post.js
@@ -19,70 +19,80 @@ import useShowToast from "../hooks/useShowToast";
 
 const Post = ({ post, postedBy }) => {
   const [liked, setLiked] = useState(false);
-  const [user, setUser] = useState(null)
-  const showToast = useShowToast()
+  const [user, setUser] = useState(null);
+  const showToast = useShowToast();
   useEffect(() => {
     const getUser = async () => {
-        try {
-          const res = await fetch(`/api/users/profile/${postedBy}`)
-          const data = await res.json()
-          console.log(data)
-          setUser(data)
-          if(data.error) {
-            showToast("Error", data.error, "error");
-            return
-          }
-        } catch (err) {
-          showToast("Error", err.message, "error");
-          setUser(null)
-        } 
-    }
-    getUser()
-  }, [postedBy, showToast])
+      try {
+        const res = await fetch(`/api/users/profile/${postedBy}`);
+        const data = await res.json();
+        console.log(data);
+        setUser(data);
+        if (data.error) {
+          showToast("Error", data.error, "error");
+          return;
+        }
+      } catch (err) {
+        showToast("Error", err.message, "error");
+        setUser(null);
+      }
+    };
+    getUser();
+  }, [postedBy, showToast]);
 
-  if(!user) return null;
-  
+  if (!user) return null;
+
   return (
     <Link to={"/:aliumusa/post/:1"}>
       <Flex gap={3} mb={4} py={5}>
         <Flex alignItems={"center"} flexDir={"column"}>
-          <Avatar src="/directorPro.jpeg" size={"md"} name={user.name} />
+          <Avatar
+            src={user.profilePic}
+            size={"md"}
+            name={`${user.name} ${user.username}`}
+          />
           <Box w={"1px"} h={"full"} bg={"gray.light"} my={2}></Box>
           <Box pos={"relative"} w={"full"}>
-            <Avatar
-              src="/post1.png"
+            {post.replies[0] && (
+              <Avatar
+                src={post.replies[0].userProfilePic}
+                size={"xs"}
+                name="Rocco mania"
+                pos={"absolute"}
+                top={"0"}
+                left={"15px"}
+                padding={"2px"}
+              />
+            )}
+            {post.replies[1] && (
+              <Avatar
+              src={post.replies[1].userProfilePic}
               size={"xs"}
-              name="Wale Oloyin"
-              pos={"absolute"}
-              top={"0"}
-              left={"15px"}
-              padding={"2px"}
-            />
-            <Avatar
-              src="/post1.png"
-              size={"xs"}
-              name="Bella Sh,urda"
+              name="Wala Adenuga"
               pos={"absolute"}
               bottom={"0"}
               right={"-5px"}
               padding={"2px"}
-            />
-            <Avatar
-              src="/post1.png"
-              size={"xs"}
-              name="Mark Danfo Driver"
-              pos={"absolute"}
-              bottom={"0"}
-              left={"4px"}
-              padding={"2px"}
-            />
+              />
+              )}
+              {post.replies[2] && (
+                <Avatar
+                src={post.replies[2].userProfilePic}
+                size={"xs"}
+                name="Ade kunle"
+                pos={"absolute"}
+                bottom={"0"}
+                left={"4px"}
+                padding={"2px"}
+                />
+                )}
           </Box>
         </Flex>
         <Flex flex={1} flexDir={"column"} gap={2}>
           <Flex justifyContent={"space-between"} w={"full"} flex={1}>
             <Flex alignItems={"center"} w={"full"}>
               <Text>{user.name}</Text>
-              <Image src="/verified.png" ml={1} w={4} h={4} />
+              <Image src={user.profilePic} ml={1} w={4} h={4} />
             </Flex>
             <Flex
               alignItems={"center"}
